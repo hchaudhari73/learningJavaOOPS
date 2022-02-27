5b9111393c87e3323a09ee2aa50dc324c12854b0
Gitea <gitea@fake.local>
2022-02-27 14:09:50 +0530

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pen pen1 = new Pen();
+        pen1.color = "Black";
+        pen1.type = "gel";
+
+        pen1.write();
+    }
+}
---
+++ src/com/company/Pen.java
@@ -0,0 +1,10 @@
+package com.company;
+
+class Pen {
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("Pen is writing with color " + this.color + ".");
+    }
+}
